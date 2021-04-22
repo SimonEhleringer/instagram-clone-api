6073d57d6f64ec75a9da824ccb9554cc11791107
SimonEhleringer <simon.ehleringer@tuer.de>
2021-04-22 12:37:21 +0200

    Initial commit

---
+++ src/main/java/com/simonehleringer/instagramcloneapi/InstagramCloneApiApplication.java
@@ -0,0 +1,13 @@
+package com.simonehleringer.instagramcloneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramCloneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramCloneApiApplication.class, args);
+	}
+
+}
