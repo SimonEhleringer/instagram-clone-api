f90de3c4d3aa49d9de06ccb5a5c8900452b79da5
Simon Ehleringer <simonehleringer@gmail.com>
2021-04-28 21:45:35 +0200

    Begin with tests for RefreshTokenService

--- src/test/java/com/simonehleringer/instagramcloneapi/authentication/accessAndRefreshToken/refreshToken/RefreshTokenServiceTest.java
+++ src/test/java/com/simonehleringer/instagramcloneapi/authentication/accessAndRefreshToken/refreshToken/RefreshTokenServiceTest.java
@@ -1,12 +1,75 @@
 package com.simonehleringer.instagramcloneapi.authentication.accessAndRefreshToken.refreshToken;
 
+import com.simonehleringer.instagramcloneapi.user.User;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.*;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class RefreshTokenServiceTest {
+    private final LocalDateTime LOCAL_DATE_TIME = LocalDateTime.of(2000, 1, 1, 1, 1, 1);
+
+    @InjectMocks
+    private RefreshTokenService underTest;
+
+    @Mock
+    private RefreshTokenRepository refreshTokenRepository;
+
+    @Mock
+    private RefreshTokenSettings refreshTokenSettings;
+
+    @Mock
+    private Clock clock;
+
+    private Clock fixedClock;
 
     @BeforeEach
     void setUp() {
+        fixedClock = Clock.fixed(
+                LOCAL_DATE_TIME.toInstant(ZoneOffset.UTC),
+                ZoneId.of("UTC")
+        );
+
+        doReturn(fixedClock).when(clock);
+    }
+
+    @Test
+    void generateNewRefreshToken() {
+        // Arrange
+        var user = new User();
+        var lifeTime = Duration.ofDays(7);
+
+        given(refreshTokenSettings.getLifeTime()).willReturn(lifeTime);
+
+        // Act
+        underTest.generateNewRefreshToken(user);
+
+        // Assert
+        var refreshTokenArgumentCaptor = ArgumentCaptor.forClass(RefreshToken.class);
+
+        verify(refreshTokenRepository).save(refreshTokenArgumentCaptor.capture());
+
+        // TODO: Test, generated Id
+
+        var capturedRefreshToken = refreshTokenArgumentCaptor.getValue();
+
+        assertThat(capturedRefreshToken.getCreationTime()).isEqualTo(LOCAL_DATE_TIME);
+        assertThat(capturedRefreshToken.getExpiryTime()).isEqualTo(LOCAL_DATE_TIME.plusDays(lifeTime.toDays()));
+        assertThat(capturedRefreshToken.getUser()).isSameAs(user);
     }
 }
