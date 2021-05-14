72c6415dd41c0800bce8ba69dc58d4d8c33240e5
SimonEhleringer <simon.ehleringer@tuer.de>
2021-05-14 12:52:28 +0200

    Add CORS configuration

--- src/main/java/com/simonehleringer/instagramcloneapi/common/config/SecurityConfig.java
+++ src/main/java/com/simonehleringer/instagramcloneapi/common/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package com.simonehleringer.instagramcloneapi.common.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -13,6 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
@@ -30,5 +38,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 "/swagger-ui.html",
                 "/swagger-ui/**"
         );
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        var config = new CorsConfiguration();
+
+        config.setAllowedOriginPatterns(Collections.singletonList("/**"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+
+        var source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }
---
+++ src/main/java/com/simonehleringer/instagramcloneapi/common/config/WebConfig.java
@@ -0,0 +1,6 @@
+package com.simonehleringer.instagramcloneapi.common.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class WebConfig extends WebMvcConfigurerAdapter {
+}
